f0179de93b098e38b8980197ac12c5d14a55271c
Narinder <programmer.narinder.singh@gmail.com>
2021-10-18 17:24:18 +1300

    multiple values through Props

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App(){
         
             
             
-            <Student name={"Anil"}Email={"programmer.narinder.singh@gmail.com"}/>
-            <Student name={"peter"}Email={"programmer.peter.singh@gmail.com"}/>
-            <Student name={"Mark"}Email={"programmer.Mark.singh@gmail.com"}/>
+            <Student name={"Anil"}Email={"programmer.narinder.singh@gmail.com"} other={{address: "Auckland",mobil:"0226439345"}}/>
+            <Student name={"peter"}Email={"programmer.peter.singh@gmail.com"} other={{address: "Wellington",mobil:"0226439346"}}/>
+            <Student name={"Mark"}Email={"programmer.Mark.singh@gmail.com"} other={{address: "Te Puke",mobil:"0226439456"}}/>
         </div>
     );
 }
--- src/User.js
+++ src/User.js
@@ -4,6 +4,7 @@ function Student(p){
     return( 
         <div><h1>Hello {p.name}</h1>
         <h2>Email :{p.Email}</h2>
+        <h4>Address:{p.other.address}</h4>
         </div>
     )
 
